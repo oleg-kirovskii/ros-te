60566956660a4b8b6b6c143ef1569f1891ac75f2
Oleg Kirovskii <oleg.kirovskii@gmail.com>
2025-08-19 22:31:58 +0400

    Fully functional rotation mechanism

--- intro_ws2/src/safe_turtle/safe_turtle/border_watch.py
+++ intro_ws2/src/safe_turtle/safe_turtle/border_watch.py
@@ -3,6 +3,8 @@ from rclpy.node import Node
 import math
 
 from turtlesim.msg import Pose
+from rclpy.action import ActionClient
+from turtlesim.action import RotateAbsolute
 
 class BorderWatch(Node):
     def __init__(self):
@@ -14,8 +16,26 @@ class BorderWatch(Node):
             10
         )
         self.subscription
+        self.get_logger().info("Border Watch Node has been started.")
+        # Create an action client for RotateAbsolute action
+        self._action_client = ActionClient(self, RotateAbsolute, '/turtle1/rotate_absolute')
+        self.get_logger().info("Action client for RotateAbsolute has been created.")
+
+    def rotate_turtle(self, theta, angle):
+        # Sends a goal to rotate the turtle to a specific angle.
+        goal_msg = RotateAbsolute.Goal()
+        goal_msg.theta = theta+angle
+        self._action_client.wait_for_server()
+        self._action_client.send_goal_async(goal_msg)
+        self.get_logger().info(f"Sent goal to rotate turtle to {angle} radians.")
 
     def time_to_collision(self,msg):
+        # Calculates the time to collision with the border of the turtlesim world.
+        # The world is a square with corners at (0,0), (10,0), (10,10), and (0,10).
+        # The turtle's position is given by (msg.x, msg.y) and its orientation by msg.theta.
+        # The turtle's linear velocity is given by msg.linear_velocity.
+        # Returns the time to collision with the border.
+        # If the turtle is not moving, returns a large number (1000.0).
         PI = math.pi
         X_MAX = 10.0
         Y_MAX = 10.0
@@ -41,7 +61,13 @@ class BorderWatch(Node):
         y = msg.y
         theta = msg.theta
         ttc = self.time_to_collision(msg)
-        self.get_logger().info("D: %f" % ttc)
+        # self.get_logger().info("D: %f" % ttc)
+        if ttc < 1.0:
+            self.get_logger().info("Turtle is close to the border, rotating to avoid collision.")
+            # Rotate the turtle to avoid collision with the border.
+            self.rotate_turtle(theta, math.pi/4)  # Rotate 45 degrees to the right
+        # else:
+        #    self.get_logger().info("Turtle is safe from the border.")
 
 def main(args=None):
     rclpy.init(args=args)
